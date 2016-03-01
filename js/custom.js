d15d0ca03469ffb3fdfe56632b4a02c92b9da23a
Manu <manu@snapdragon.cc>
2016-03-01 23:04:13 +0800

    Fix error message after form submission.

--- js/custom.js
+++ js/custom.js
@@ -57,7 +57,7 @@
 		// Home page forms
 		$('form.de input[type="submit"]').click(function(){
 			$.post("//formspree.io/postmaster@aeronimo.com", $("#contactform").serialize(),  function() {
-				$('#message').html('Danke für Ihre Anmeldung. Sie hören von uns.');
+				$('.text-msg').html('Danke für Ihre Anmeldung. Sie hören von uns.');
 				$( "#message" ).show(1000);
 			}, "json");
 			return false;
@@ -65,7 +65,7 @@
 
 		$('form.en input[type="submit"]').click(function(){
 			$.post("//formspree.io/postmaster@aeronimo.com", $("#contactformEn").serialize(),  function() {
-				$('#message').html('Thanks for signing up. We will get back to you soon.');
+				$('.text-msg').html('Thanks for signing up. We will get back to you soon.');
 				$( "#message" ).show(1000);
 			}, "json");
 			return false;
@@ -75,7 +75,7 @@
 		$('form.de #submit-btn').click(function(){
 
 			$.post("//formspree.io/postmaster@aeronimo.com", $("#registerDE").serialize(),  function(response) {
-				$('#contact-message').html('Danke für Ihre Anmeldung. Sie hören von uns.');
+				$('.text-msg').html('Danke für Ihre Anmeldung. Sie hören von uns.');
 				$( "#contact-message" ).show(1000);
 			}, "json");
 			return false;
@@ -85,7 +85,7 @@
 		$('form.en #submit-btnEn').click(function(){
 
 			$.post("//formspree.io/postmaster@aeronimo.com", $("#registerEN").serialize(),  function(response) {
-				$('#contact-message').html('Thanks for signing up. We will get back to you soon.');
+				$('.text-msg').html('Thanks for signing up. We will get back to you soon.');
 				$( "#contact-message" ).show(1000);
 			}, "json");
 			return false;
